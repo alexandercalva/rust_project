42b34cd43dfe05b82b08c445da7669b36ffdf3b8
calva-alexander <38995873+alexandercalva@users.noreply.github.com>
2022-12-11 21:24:15 -0500

    Hello World

---
+++ helloworld/target/debug/deps/helloworld.d
@@ -0,0 +1,5 @@
+C:\Users\Alexander\Documents\BYU\BYU-Alex\CSE310\rust_project\helloworld\target\debug\deps\helloworld.exe: src\main.rs
+
+C:\Users\Alexander\Documents\BYU\BYU-Alex\CSE310\rust_project\helloworld\target\debug\deps\helloworld.d: src\main.rs
+
+src\main.rs:
---
+++ helloworld/target/debug/helloworld.d
@@ -0,0 +1,1 @@
+C:\Users\Alexander\Documents\BYU\BYU-Alex\CSE310\rust_project\helloworld\target\debug\helloworld.exe: C:\Users\Alexander\Documents\BYU\BYU-Alex\CSE310\rust_project\helloworld\src\main.rs
